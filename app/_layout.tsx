fa45e9d45df2831f83d994bc0ca3bcfd56f68c00
noel kofmel <noel.kofmel@swisscom.com>
2025-04-23 14:12:35 +0200

    style: some style changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './screens/main/MainScreen';
-// import ScanScreen from './screens/Scan/ScanScreen';
+// import LoginScreen from './screens/auth/LoginScreen';
+// import AccountScreen from './screens/Account/AccountScreen';
+// import ScanScreen from './screens/scan/ScanScreen';
 
 const Stack = createStackNavigator();
 
 export default function Layout() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="MainScreen" component={MainScreen} />
-      {/* <Stack.Screen name="ScanScreen" component={ScanScreen} /> */}
+      <Stack.Screen 
+        name="MainScreen" 
+        component={MainScreen}
+        options={{ title: 'Quitify Übersicht' }}
+      />
+      {/* <Stack.Screen 
+        name="LoginScreen" 
+        component={LoginScreen}
+        options={{ title: 'Login' }}
+      />
+      <Stack.Screen 
+        name="AccountScreen" 
+        component={AccountScreen}
+        options={{ title: 'Mein Account' }}
+      /> */}
     </Stack.Navigator>
   );
 }
--- app/screens/main/MainScreen.tsx
+++ app/screens/main/MainScreen.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import  Header from '../../../components/Header';
-
+import React from "react";
+import { View, Text, Button } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import styles from "./MainScreen.styles";
+import Header from "../../../components/Header";
 
 type RootStackParamList = {
   MainScreen: undefined;
   ScanScreen: undefined;
 };
 
-type MainScreenNavigationProp = StackNavigationProp<RootStackParamList, 'MainScreen'>;
+type MainScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "MainScreen"
+>;
 
 interface MainScreenProps {
   navigation: MainScreenNavigationProp;
@@ -18,47 +21,23 @@ interface MainScreenProps {
 const MainScreen: React.FC<MainScreenProps> = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Header navigation={navigation} />
+      {/* <Header navigation={navigation} /> */}
       <Text style={styles.header}>Willkommen zu Quitify!</Text>
       <Text style={styles.body}>
-        Scanne deine Quittungen, um sie zu speichern und später darauf zuzugreifen.
+        Scanne deine Quittungen, um sie zu speichern und später darauf
+        zuzugreifen.
       </Text>
 
       <Button
         title="Quittung Scannen"
-        onPress={() => navigation.navigate('ScanScreen')} 
+        onPress={() => navigation.navigate("ScanScreen")}
       />
 
-      <Text style={styles.footer}>Erstelle, speichere und verwalte deine Quittungen.</Text>
+      <Text style={styles.footer}>
+        Erstelle, speichere und verwalte deine Quittungen.
+      </Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-    backgroundColor: '#fff',
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    color: '#000000',
-  },
-  body: {
-    fontSize: 18,
-    textAlign: 'center',
-    marginBottom: 20,
-    color: '#000000',
-  },
-  footer: {
-    marginTop: 30,
-    fontSize: 14,
-    color: '#000000',
-  },
-});
 
 export default MainScreen;
